76aa1f194df46d07f2524d182bec9a78ac1b1b8f
krukovits <krukovits@ukr.net>
2024-02-11 22:30:55 +0200

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace Task2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.InputEncoding = Encoding.Unicode;
+			Console.OutputEncoding = Encoding.Unicode;
+
+			int[] input = { 1, 2, 3, 4, 4, 56 };
+			int[] temp = new int[input.Length];
+			int counterTemp = 0;
+			temp[counterTemp] = input[0];
+			
+			for (int i = 1; i < input.Length; i++)
+			{
+				if (input[i - 1] == input[i])
+				{
+					continue;
+				}
+				else 
+				{
+					counterTemp++;
+					temp[counterTemp] = input[i];
+				}
+			}
+
+			int[] output = new int[counterTemp + 1];
+			Array.Copy(temp, output, counterTemp + 1);
+
+			foreach (int i in output)
+			{
+				Console.WriteLine(i);
+			}
+			Console.ReadKey();
+		}
+	}
+}
